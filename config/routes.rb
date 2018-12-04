0c3b62be3b485ffc494d1f6763c64e0b05469dc2
gavinmoore36 <gavizoid16@gmail.com>
2018-12-04 17:45:14 -0500

    moe changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users, only:[:show, :new, :create, :edit, :update]
   resources :museums, only:[:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/queues', to: 'queue#index', as: "queues_index"
+  patch '/queue', to: 'queue#update', as: "add_museum_to_queue"
 end
