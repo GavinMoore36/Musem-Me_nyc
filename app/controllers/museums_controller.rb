f8cbb4ec4f0ba9714c9d61fda23d87bbb3922da9
gavinmoore36 <gavizoid16@gmail.com>
2018-12-04 09:13:21 -0500

    beginning of view and controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,2 +1,18 @@
 class MuseumsController < ApplicationController
+  before_action :find_museum, only: :show
+
+  def index
+    @museums = Museum.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def find_museum
+    @museum = Museum.find(params[:id])
+  end
+
 end
