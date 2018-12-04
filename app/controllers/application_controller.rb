e84144c9f088a3932c5247f5a519001613f5fce4
Connor Callahan <connorwcallahan@gmail.com>
2018-12-04 17:22:29 -0500

    added sessions and queue controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def queue
+    session[:museum_id] ||= []
+  end
+
+  def add_museum_to_queue(museum_id)
+    queue << museum_id
+  end
+
+  def display_queue
+    @queue_museums = Museum.find(queue)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :museums
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/queues', to: 'queue#index', as: "queues_index"
+  patch '/queue', to: 'queue#update', as: "add_museum_to_queue"
 end
