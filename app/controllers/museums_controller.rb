abf870b35912c71b6e984ceadddbb98414671178
Connor Callahan <connorwcallahan@gmail.com>
2018-12-05 18:58:53 -0500

    started search functionality and custom route for homepage

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -7,11 +7,22 @@ class MuseumsController < ApplicationController
 
   def show
   end
+  def search
+    if Museum.search(params[:search]) == @museum
+      redirect_to museum_path(@museum)
+    else
+      redirect_to home_index_path
+    end
+  end
 
   private
 
   def find_museum
     @museum = Museum.find(params[:id])
+  end
+
+  def museum_params
+    params.require(:museum).permit(:title, :address, :coordinate_x, :coordinate_y, :url, :zip, :phone, :borough_id, :search)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :users, only:[:show, :new, :create, :edit, :update]
   resources :museums, only:[:index, :show]
   resources :boroughs, only:[:index, :show]
+  get '/', to: 'application#welcome', as: "home_index"
+  post '/search', to: 'museums#search', as: "search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/queues', to: 'queues#index', as: "queues_index"
+  get '/queues', to: 'queues#index', as: "queues_index_refresh"
   patch '/queue', to: 'queues#update', as: "add_museum_to_queue"
 end
