0c5b26bcb7bef36af7b636e931e88789176c70ea
Connor Callahan <connorwcallahan@gmail.com>
2018-12-06 18:36:36 -0500

    styling on all pages

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -1,6 +1,7 @@
 class QueuesController < ApplicationController
 
   def update
+
     flash[:message] = "You added #{params[:museum_title]} to your queue."
     add_museum_to_queue(params[:museum_id])
     redirect_to museums_path
