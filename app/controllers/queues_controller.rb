b469183ab2c597a9aa89bf7672b6d8c59d856bce
Connor Callahan <connorwcallahan@gmail.com>
2018-12-06 21:06:51 -0500

    added styling to the index page for add button

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -1,7 +1,6 @@
 class QueuesController < ApplicationController
 
   def update
-
     flash[:message] = "You added #{params[:museum_title]} to your queue."
     add_museum_to_queue(params[:museum_id])
     redirect_to museums_path
